81e77cd73a0681b01b3666399f33de9836f885b4
Luc4st1574 <n00281417@upn.pe>
2024-04-28 10:12:38 -0500

    add: logic layer

---
+++ CapaNegocio/CN_Reporte.cs
@@ -0,0 +1,28 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Reporte
+    {
+        private CD_Reporte objcd_reporte = new CD_Reporte();
+
+        public List<ReporteCompra> Compra(string fechainicio, string fechafin, int idproveedor)
+        {
+            return objcd_reporte.Compra(fechainicio, fechafin, idproveedor);
+        }
+
+
+        public List<ReporteVenta> Venta(string fechainicio, string fechafin)
+        {
+            return objcd_reporte.Venta(fechainicio, fechafin);
+        }
+    }
+}
---
+++ CapaNegocio/CN_Usuario.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using CapaDatos;
+using System.Security.Policy;
+
+namespace CapaNegocio
+{
+    public class CN_Usuario
+    {
+
+        private CD_Usuario cdUsuario = new CD_Usuario();
+
+        public List<Usuario> Listar()
+        {
+            return cdUsuario.Listar();
+        }
+
+        public int Registrar(Usuario obj, out string Mensaje) 
+        {
+            Mensaje = "";
+            if (obj.Documento == "")
+            {
+                Mensaje = "El campo Documento es obligatorio";
+                
+            }
+            if (obj.NombreCompleto == "")
+            {
+                Mensaje = "El campo Nombre Completo es obligatorio";
+               
+            }
+            if (obj.Correo == "")
+            {
+                Mensaje = "El campo Correo es obligatorio";
+               
+            }
+            if (obj.Clave == "")
+            {
+                Mensaje = "El campo Clave es obligatorio";
+                
+            }
+            if (obj.oRol.IdRol == 0)
+            {
+                Mensaje = "El campo Rol es obligatorio";
+                
+            }
+
+            if (Mensaje != string.Empty) 
+            {
+                return 0;
+            }
+            else
+            {
+                return cdUsuario.Registrar(obj, out Mensaje);
+            }
+
+        }
+        
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = "";
+            if (obj.Documento == "")
+            {
+                Mensaje = "El campo Documento es obligatorio";
+
+            }
+            if (obj.NombreCompleto == "")
+            {
+                Mensaje = "El campo Nombre Completo es obligatorio";
+
+            }
+            if (obj.Correo == "")
+            {
+                Mensaje = "El campo Correo es obligatorio";
+
+            }
+            if (obj.Clave == "")
+            {
+                Mensaje = "El campo Clave es obligatorio";
+
+            }
+            if (obj.oRol.IdRol == 0)
+            {
+                Mensaje = "El campo Rol es obligatorio";
+
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return cdUsuario.Editar(obj, out Mensaje);
+            }
+        }
+
+        public bool Eliminar(Usuario obj, out string Mensaje)
+        {
+            return cdUsuario.Eliminar(obj, out Mensaje);
+        }
+
+        public void loggedtrue(Usuario obj)
+        {
+            cdUsuario.ActualizarLoggedTrue(obj);
+        }
+
+        public void loggedfalse(Usuario obj)
+        {
+            cdUsuario.ActualizarLoggedFalse(obj);
+        }
+    }
+}
